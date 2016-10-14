ec1af9c78196cf8e566b91553eb4bad04038edce
Nicko <nicolasmathieu31@hotmail.com>
2016-10-14 12:32:56 +0200

    countDown clearInterval

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 
-var timer = 60;
+var timer = 90;
+var stop;
 
 
-setInterval(function(){
-$(".affichage").text(timer--);
+function secondes(){
+stop = setInterval(function(){
+	$(".secondes").text(timer--);
+	if(timer<0){
+		clearInterval(stop);
+	}
 },1000);
+
+
+}
+
+function minutes(){
+stop = setInterval(function(){
+	$(".minutes").text(timer--);
+	if(timer<0){
+		clearInterval(stop);
+	}
+},1000);
+
+
+}
+
+function heures(){
+stop = setInterval(function(){
+	$(".heures").text(timer--);
+	if(timer<0){
+		clearInterval(stop);
+	}
+},1000);
+
+
+}
+
+
+
+/*$("h2").text(heures()+":"+minutes()+":"+secondes());*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

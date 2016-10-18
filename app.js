098d46fa474beb5b6ac6d67734bd2e615825b986
Nicko <nicolasmathieu31@hotmail.com>
2016-10-18 16:51:43 +0200

    CountDown fini

--- app.js
+++ app.js
@@ -10,10 +10,10 @@
 			
 		},
 		listeners : function(){
+			$("#butval").on('click', this.done.bind(this));
 			$("#start").on('click', this.start.bind(this));
 			$("#pause").on('click', this.pause.bind(this));
 			$("#reset").on('click', this.reset.bind(this));
-			$("#butval").on('click', this.done.bind(this));
 
 		},
 		start : function(){
@@ -26,16 +26,13 @@
 			clearInterval(this.intervalID);
 
 		},
-		reset : function(){
-			this.init();
-		},
 		decrement : function(){
 			this.timer--;
 			console.log(this.timer);
 			this.updateView();
 			if(this.timer <= 0){
 				clearInterval(this.intervalID);
-
+				
 			}
 		},
 		done : function(){
@@ -43,27 +40,30 @@
 			var sec = parseInt($("#inputsec").val(),10);
 			$("#minutes").html(min);
 			$("#secondes").html(sec);
+			if(isNaN(min)){
+				min = 0;
+			}if(isNaN(sec)){
+				sec = 0;
+			}
 			this.timer = min*60 + sec;
-
+		},
+		reset : function(){
+			this.done();
+			console.log("braaapp");
+			
 		},
 		updateView : function(){
-			
+
 			var min= parseInt(this.timer / 60, 10);
 			var sec= this.timer % 60;
 			$("#minutes").html(min);
 			$("#secondes").html(sec);
-
 			if(min < 10){
 				$("#minutes").html("0" + min);
-			}
-
-			if(sec < 10){
+			}if(sec < 10){
 				$("#secondes").html("0" + sec);
 			}
-
-
 		},
-
 
 	};
 	app.init();

09a3e569f273b1fa24c39516aea8a37d0e563723
Nicko <nicolasmathieu31@hotmail.com>
2016-10-18 10:31:23 +0200

    countDown avec this

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-var temps = 240;
-var pause;
+"use strict";
 
-function decompte(){
-	pause = setInterval(function(){
-		temps--;
-		var minutes = parseInt(temps / 60,10);
-		var secondes = temps - minutes*60;
-		$("#minutes").text(minutes);
-		$("#secondes").text(secondes);
 
-		if(temps <= 0){
-			clearInterval(pause);
-		}
-	},1000);
-};
-$("button").click(function(){
-	clearInterval(pause);
-});
-decompte();
+(function(){
+	var app = {
+		time : 60,
+		intervalID : null,
+		init : function(){
+			app.listeners();
+		},
+		listeners : function(){
+			$("#start").on('click', app.start);
+			$("#pause").on('click', app.pause);
+			$("#reset").on('click', app.reset);
+			$("#butval").on('click',app.inputTemps);
+			
+		},
+		start : function(){
+			clearInterval(app.intervalID);
+			app.intervalID = setInterval(app.decrement, 
+				1000);
+			
+		},
+		pause : function(){
+			clearInterval(app.intervalID);
+
+		},
+		reset : function(){
+
+			app.time = 60;
+		},
+		decrement : function(){
+			app.time--;
+			app.vueSablier();
+			if(app.time <= 0){
+				clearInterval(app.intervalID);
+			}
+		},
+		vueSablier : function(){
+			var minutes = parseInt(app.time / 60, 10);
+			var secondes = app.time % 60;
+			var inputMin = $("#min").val();
+			
+			$("#minutes").html(minutes);
+			$("#secondes").html(secondes);
+			
+
+		},
+
+	};
+	app.init();
+})();
+			/*if (minutes <10 && secondes <10){
+				$("#nbre").html("0" + minutes + ":" + "0" + secondes);
+
+			}*/
